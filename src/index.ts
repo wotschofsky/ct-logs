ea808b9df3d8a292d2d8ff77776887b618dc3bbf
Felix Wotschofsky <felix@wotschofsky.com>
2024-05-21 14:37:51 +0000

    Auto exit app if no messages come in

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import 'dotenv/config';
 import { CertStreamClient } from './cert-stream';
 import { addDomain } from './mongo';
 
+let lastMessageAt = Date.now();
+
 let client = new CertStreamClient(meta => {
   const { all_domains } = meta.data.leaf_cert
   if (!all_domains) return
   console.log('Logging domains', all_domains)
   all_domains?.forEach(addDomain)
+  lastMessageAt = Date.now();
 });
 
 console.info('> started');
@@ -18,3 +21,10 @@ console.info('> connected to stream');
 
 // Failed to connect
 if (!client.ws) process.exit(0);
+
+setInterval(() => {
+  if (Date.now() - lastMessageAt > 30_000) {
+    console.error('No messages received in 30 seconds, exiting...');
+    process.exit(0);
+  }
+}, 1000);
